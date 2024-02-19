44c97262028ec511c18d567b555352f8f12c6dca
Murilo <lilo.marino@hotmail.com>
2024-02-18 21:59:35 -0300

    Refatorado config.py

--- config.py
+++ config.py
@@ -1,11 +1,36 @@
 import logging
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# Logging Config
 logging.basicConfig(level=logging.INFO)
-ROOT_FOLDER = Path(__file__).parent
-CHROMEDRIVER_EXEC = ROOT_FOLDER / "driver" / "chromedriver.exe"
-TIME_TO_WAIT = 30
-TIME_ROLL = 1
+
+# Login Info
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+# Discord IDs
+CHANNEL_ID = os.getenv("CHANNEL_ID")  # ID of claiming channel
+SERVER_ID = os.getenv("SERVER_ID")  # ID of Discord server
+
+# Command prefix for Mudae and roll command to use.
 COMMAND_PREFIX = "$"
 ROLL_COMMAND = "wa"
-HEADLESS = False
+
+# Timers
+TIME_TO_WAIT = 30
+TIME_ROLL = 1
+
+# Emoji used for claiming
+CLAIM_EMOJI = ":heart:"
+CLAIM_METHOD_CLICK = False  # If True claim will attempt to react on emoji instead of add one (Needs Emoji available)
+
+# SELENIUM CONFIG INFO #
+ROOT_FOLDER = Path(__file__).parent
+CHROMEDRIVER_EXEC = ROOT_FOLDER / "driver" / "chromedriver.exe"
+
+# | DEBUGGING TOOLS | #
+HEADLESS = False  
